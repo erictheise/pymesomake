df54d4b7b5bfec7b1877c25e41c81237d6b1e81c
Eric Theise <erictheise@gmail.com>
2019-01-30 22:57:14 -0800

    Attempts to capture poem text and console print for cli.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pymesomake",
-    version="0.3.3",
+    version="0.4.0",
     author="Eric Theise",
     author_email="erictheise@gmail.com",
     description="An implementation of the mesostic generation algorithm Andrew Culver developed for John Cage.",
